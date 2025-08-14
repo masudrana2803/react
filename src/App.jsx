194e04a589df02326f3130b88170be61d61f0afc
masudrana2803 <masudrana2803@gmail.com>
2025-08-14 11:49:13 +0600

    filesCreated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './assets/react.svg'
+
+
+import './App.css'
+// import './components/Aboutme'
+import './components/Banner'
+import './components/Footer'
+import './components/Navbar'
+import './components/Aboutme'
+
+
 
 const App = () => {
   return (
     <div>
       <h1>Hello</h1>
+      <h2>I am doing react</h2>
     </div>
   )
 }
