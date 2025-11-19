b16a58058f5942595727b62664de45592e802d9d
masudrana2803 <masudrana2803@gmail.com>
2025-11-19 22:24:12 +0600

    Prodcuts lists

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,16 +1,34 @@
 // src/pages/Products.jsx
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
+import { data } from 'react-router';
 
 
 const Products = () => {
 
   const [products, setProducts] = useState([]);
 
-  fetch('https://dummyjson.com/products')
-.then((res) => res.json())
-.then((data)=>console.log(data));
+useEffect(() => {
+
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products)) // Save products array
+      .catch((err) => console.error('Failed to fetch products:', err));
+      console.log(products)
+
+}, []);
+
+useEffect(() => {
+    console.log(products)
+}, [products]);
+
+
+
+
+
+
 
 /*
+
 useEffect(() => {
   // 1️⃣ Fetch data from DummyJSON API when the component mounts
   fetch('https://dummyjson.com/products')
@@ -24,7 +42,10 @@ useEffect(() => {
 
 // 7️⃣ This console.log runs every time the component re-renders
 console.log(products);
+
 */
+
+
 
   return (
     <>
@@ -38,12 +59,26 @@ console.log(products);
       </div>
     <div className='m-4'>
       <h2 className='p-2'>List of Products</h2>
-      <div className='bg-gray-400 w-50 h-50 p-2'>
-        <img src="afds" alt="" />
+      <div className='products flex gap-4 flex-wrap'>
+        {products.map((product)=>(
+        
+        
+        <div className='m-4'>
+          <div className='bg-gray-100 w-50 h-50 p-2'>
+            <img 
+            src={product.thumbnail} 
+            alt={product.title}
+            />
+          </div>
+          <h2>Title: {product.title}</h2>
+          <h2>Price: {product.price} $</h2>
+        </div>
+        ))}
+
       </div>
-      <h2>Title:</h2>
-      <h2>Price:</h2>
     </div>
+
+
     </div>
     </>
   )
