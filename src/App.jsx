e0ec450e3cbda7e9eefcdb9e45ce1f7b1b2ee785
masudrana2803 <masudrana2803@gmail.com>
2025-11-29 10:57:23 +0600

    npm run build

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,44 @@
-// src/App.jsx
 import React from 'react'
-import './assets/react.svg'
 import './App.css'
-
-
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Banner from './components/Banner'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contacts from './pages/Contacts'
-import Services from './pages/Services'
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
 import LayoutOne from './Layout/LayoutOne'
 import LayoutTwo from './Layout/LayoutTwo'
+import Banner from './components/Banner'
+import Home from './pages/Home'
+import About from './pages/About'
 import Products from './pages/Products'
 import Buttons from './pages/Buttons'
 import States from './pages/States'
+import Contacts from './pages/Contacts'
+import Services from './pages/Services'
 
 
 
 
 const App = () => {
-
   const myRoute = createBrowserRouter(createRoutesFromElements(
     <Route>
-      <Route path='/'element={<LayoutOne/>}>
-      <Route index element={<Banner/>}/>
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/Products' element={<Products/>}/>
-
-      <Route path='/Buttons' element={<Buttons/>}/>
-      <Route path='/States' element={<States/>}/>
-
-      
-      {/* <Route path='/Contacts' element={<Contacts/>}/>
-      <Route path='/Services' element={<Services/>}/> */}
+      <Route path='/' element={<LayoutOne/>}>
+        <Route index element={<Banner/>}/>
+        <Route path='/Home' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Products' element={<Products/>}/>
+        <Route path='/Buttons' element={<Buttons/>}/>
+        <Route path='/States' element={<States/>}/>
       </Route>
-      {/* <Route path='/' element={<Banner/>}/>
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/Contacts' element={<Contacts/>}/>
-      <Route path='/Services' element={<Services/>}/> */}
-
-      <Route path='/'element={<LayoutTwo/>}>
-      {/* <Route index element={<Banner/>}/>
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/About' element={<About/>}/> */}
-      <Route path='/Contacts' element={<Contacts/>}/>
-      <Route path='/Services' element={<Services/>}/>
-      <Route path='/Products' element={<Products/>}/>
-
+      <Route path='/' element={<LayoutTwo/>}>
+        <Route path='/Contacts' element={<Contacts/>}/>
+        <Route path='/Services' element={<Services/>}/>
+        <Route path='/Products_alt' element={<Products/>}/>
       </Route>
-
     </Route>
-
-
-
-
-
-
   ))
 
   return (
     <>
-    <RouterProvider router={myRoute}/>
-    {/* <Navbar/>
-    <Banner/>
-    <Footer/>
-    <Home/>
-    <About/>
-    <Contacts/>
-    <Services/> */}
-
+      <RouterProvider router={myRoute}/>
     </>
-    // <div className="bodyText">
-    //   <h1>Hello</h1>
-    //   <h2>I am doing react</h2>
-    // </div>
   )
 }
 
